d6d55221463c88bf5c28391ea5c01aba334084fc
Rahul P <rahulratnakaran463@gmail.com>
2024-05-07 22:11:37 +0530

    Pieces added to project

---
+++ Chess/piece_King.hpp
@@ -0,0 +1,16 @@
+#include "Piece.hpp"
+
+
+class King: public Piece
+{
+    bool castlingDone = false;
+public:
+    King(bool white);
+    bool isCastlingDone();
+    void setCastlingDone(bool castlingDone);
+    bool canMove(Board* board, Spot* start, Spot* end) override;
+    void move(Board* board, Spot* start, Spot* end) override;
+    bool isValidCastling(Board* board, Spot* start, Spot* end);
+    bool isCastlingMove(Spot* start, Spot* end);
+    string getSymbol();
+}
---
+++ Chess/piece_queen.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "piece.hpp"
+
+class Queen : public Piece {
+public:
+    Queen(bool white);
+    bool canMove(Board* board, Spot* start, Spot* end) override;
+    void move(Board* board, Spot* start, Spot* end) override;
+    string getSymbol();
+};
